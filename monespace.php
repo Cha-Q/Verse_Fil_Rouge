9c7967a4ef77fe2c1212193ffe4d09f53b628958
Cha-Q <quentin.chassaing@outlook.com>
2022-05-09 11:47:52 +0200

    12am

--- monespace.php
+++ monespace.php
@@ -20,21 +20,21 @@
         <p> Powered by <a href="www.twitch.com">Twitch</a><i class="fab fa-twitch"></i></p>
     </header>
 
-	<h2 >Bienvenue dans ton Verse <?= $_SESSION['login']; ?></h2>
+	<h2 style="color:aliceblue;" >Bienvenue dans ton Verse <?= $_SESSION['login']; ?></h2>
 
     <nav class="navbar navbar-expand-lg bg-light">
 
         <a class="navbar-brand" href="./index.html">Verse</a>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 
             <span class="navbar-toggler-icon"></span>
 
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-            <ul class="navbar-nav">
+            <ul class="navbar-nav mr-auto">
 
                 <li class="nav-item active">
                     <a class="nav-link" href="./infoCompte.php">Mon compte</a>
@@ -45,11 +45,14 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Mes amis</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
 
             </ul>
-                <form method="POST">
-                    <input value="deconnect" class="btn btn-navbar" name="deconnect" type="submit"   >se déconnecter </input>
-                    </form>
+            <form method="POST" >
+                <input value="se deconnecter" id="deconnect" class="btn btn-navbar" name="deconnect" type="submit"></input>
+            </form>
         </div>
         
     </nav>
@@ -68,7 +71,9 @@
         <span id="player"></span>
     </div>
     
-
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
     <script type="text/javascript" src="./player.js">
 
     </script>
